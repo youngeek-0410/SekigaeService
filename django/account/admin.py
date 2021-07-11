f5319714912c64d60e254a36cc1b5e94d11776e9
kathmandu777 <mahhakatomanato@gmail.com>
2021-07-11 23:47:08 +0900

    create user model #16

--- django/account/admin.py
+++ django/account/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(User)
+class AdminUserAdmin(UserAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('email',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_filter = ('is_admin',)
+    list_display = ('email',)
+    search_fields = ('username', 'email',)
+    ordering = ('date_joined',)
+    filter_horizontal = ('groups', 'user_permissions',)
--- django/account/models.py
+++ django/account/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+import uuid as uuid_lib
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, username, uuid):
+        if not uuid:
+            raise ValueError('Uuid is required in order to create user.')
+        user = self.model(email=email, username=username, uuid=uuid)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.model(email=email)
+        user.is_admin = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    uuid = models.UUIDField(default=uuid_lib.uuid4,
+                            editable=False, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(
+        _('username'),
+        max_length=250,
+        blank=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
