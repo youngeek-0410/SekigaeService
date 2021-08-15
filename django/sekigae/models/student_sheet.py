7979322d71657bc63db2bb6892fc99426a7f0b84
is-hoku <this@is-hoku.info>
2021-08-15 13:06:06 +0900

    add Student and StudentSheet models #19

---
+++ django/sekigae/models/student_sheet.py
@@ -0,0 +1,17 @@
+from django.db import models
+from account.models import User
+from django.utils import timezone
+
+class StudentSheet(models.Model):
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=255)
+	created_at = models.DateTimeField(default=timezone.now)
+	updated_at = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = "student_sheet"
+		verbose_name_plural = "student_sheets"
+
