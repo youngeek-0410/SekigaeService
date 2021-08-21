e97b1d8820fbfc1e9cc1c0e504c952f289722ae4
is-hoku <this@is-hoku.info>
2021-08-21 15:54:03 +0900

    format

--- django/sekigae/views/user.py
+++ django/sekigae/views/user.py
@@ -2,23 +2,26 @@ from django.views.generic import TemplateView, DeleteView
 from ..account.models import User
 from django import http
 
+
 class UserGetView(TemplateView):
-	template_name = 'account/base_account.html'
-	def get_context_data(self, **kwargs):
-		user = self.request.user
-		context = super().get_context_data(**kwargs)
-		context['account'] = user
-		return context
+    template_name = 'account/base_account.html'
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        context = super().get_context_data(**kwargs)
+        context['account'] = user
+        return context
 
 
 class UserDeleteView(DeleteView):
-	model = User
-	success_url = '/path/to/success/url'
-	def delete(self, request, *args, **kwargs):
-		self.object = self.get_object()
-		if self.object.User == request.user:
-			success_url = self.get_success_url()
-			self.object.delete()
-			return http.HttpResponceRedirect(success_url)
-		else:
-			return http.HttpResponceForbidden("cannot delete")
+    model = User
+    success_url = '/path/to/success/url'
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.User == request.user:
+            success_url = self.get_success_url()
+            self.object.delete()
+            return http.HttpResponceRedirect(success_url)
+        else:
+            return http.HttpResponceForbidden("cannot delete")
