41e2999d89cab4708e54d3e2620f32f4cc975353
is-hoku <this@is-hoku.info>
2021-09-02 22:38:48 +0900

    WIP #60

--- django/account/views.py
+++ django/account/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django import http
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import User
+
+
+class UserView(LoginRequiredMixin, TemplateView):
+    template_name = "account/base_account.html"
+    model = User
+
+    # def __init__(self, *args, **kwargs):
+    # model = User
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        context = super().get_context_data(**kwargs)
+        context["account"] = user
+        return context
+
+    def get_success_url(self):
+        return reverse("sekigae:user")
+
+    def delete(self, request, *args, **kwargs):
+        success_url = self.get_success_url()
+        request.user.delete()
+        return http.HttpResponseRedirect(success_url)
