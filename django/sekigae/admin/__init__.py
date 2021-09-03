f96fef502681a17dcf6b844b985a858cb568a9b5
is-hoku <this@is-hoku.info>
2021-09-03 14:13:08 +0900

    create seat_sheet and seat_pair #21

--- django/sekigae/admin/__init__.py
+++ django/sekigae/admin/__init__.py
@@ -1,4 +1,6 @@
-from .seat_format import *
 from .seat import *
+from .seat_format import *
+from .seat_pair import *
+from .seat_sheet import *
 from .student import *
 from .student_sheet import *
