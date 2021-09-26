6e6330adb096813acaf43bb6c8c8316a177074b6
Rei <reirain256@gmail.com>
2021-09-26 23:39:29 +0900

    create stepper in dashboard
    fix import mui

--- django/sekigae/urls.py
+++ django/sekigae/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from .views import IndexView
+from .views import DashboardView
 
 app_name = 'sekigae'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('dashboard', DashboardView.as_view(), name='dashboard'),
 ]
---
+++ django/sekigae/views/dashboard.py
@@ -0,0 +1,4 @@
+from django.views.generic import TemplateView
+
+class DashboardView(TemplateView):
+    template_name = "sekigae/dashboard.html"
