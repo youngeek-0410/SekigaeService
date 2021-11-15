a3a4e8d2805daf6b6d6d27344d61599fb17ae809
kathmandu777 <mahhakatomanato@gmail.com>
2021-11-15 15:27:44 +0900

    rename: api -> app #295

--- fastapi/db/migrations/env.py
+++ fastapi/db/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 import os
-from api.models import Base
+from app.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
