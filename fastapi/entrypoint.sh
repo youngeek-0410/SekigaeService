4053e34b84590fe29cffebd1b5db163c4f63dc6f
kathmandu777 <mahhakatomanato@gmail.com>
2021-11-17 17:26:04 +0900

    feat: add user same as host pc one in docker #295

--- fastapi/entrypoint.sh
+++ fastapi/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# DBとの接続を待つ
 if [ "$DB" = "postgres" ]
 then
     echo "Waiting for postgres..."
@@ -11,11 +12,21 @@ then
     echo "PostgreSQL started"
 fi
 
+# host PCと同じユーザーを作成してpermission問題を解決
+USER_ID=${USER_ID:-1000}
+GROUP_ID=${GROUP_ID:-1000}
+USER_NAME=${USER_NAME:-developer}
+echo "Creating user($USER_NAME) with UID : $USER_ID, GID: $GROUP_ID"
+useradd -u $USER_ID -o -m $USER_NAME
+groupadd -g $GROUP_ID $USER_NAME
+
+# migration & seed
 cd /src/db
 poetry run alembic upgrade head
 poetry run python seed.py
 
+# fastapiサーバーの起動
 cd /src
 poetry run uvicorn app.main:app --host 0.0.0.0 --reload
 
-exec $cmd
+exec /usr/sbin/gosu $USER_NAME "$@"
