44bebfdcf688eab2f69b5a55382d4d27a6b6e499
kathmandu777 <mahhakatomanato@gmail.com>
2021-11-25 19:05:39 +0900

    wip

--- fastapi/app/db/migrations/env.py
+++ fastapi/app/db/migrations/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from app.db.database import DATABASE_URL, Base
 from sqlalchemy import engine_from_config, pool
+from app.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
