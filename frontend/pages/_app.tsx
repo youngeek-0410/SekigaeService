92320da13cdc3c93f3e48c65f18c392da2f3e73e
lemonadern <taishi.develop@gmail.com>
2021-11-27 01:05:45 +0900

    feat: chakra-theme and css variable settings

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { Layout } from 'page/layouts/Layout'
-import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from 'assets/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         < Component {...pageProps} />
       </Layout>
---
+++ frontend/src/assets/colors.ts
@@ -0,0 +1,24 @@
+const colors = {
+  custom: {
+    100: "lightblue",
+    900: "blue",
+  },
+}
+
+// usage below
+// ```
+//   custom: {
+//     100: "#fafafa",
+//     200: "#f7f7f7",
+//   },
+// ```
+// は、アプリ内では, 以下のように使用することができます。
+// ```
+// <Text fontColor="custom.100">color is #fafafa</Text>
+// <Text fontColor="custom.900">color is blue</Text>
+// ```
+// 
+// 詳しくはChakra UIのドキュメントを参照してください。
+// https://chakra-ui.com/docs/features/css-variables
+
+export default colors
